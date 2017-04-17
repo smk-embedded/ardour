a12a4dd4184e3341e756c1d2b8c3873bef3a9bbd
Paul Davis <paul@linuxaudiosystems.com>
2017-04-17 11:12:49 +0100

    no more per-track varispeed

--- gtk2_ardour/editor_ops.cc
+++ gtk2_ardour/editor_ops.cc
@@ -797,17 +797,6 @@ Editor::build_region_boundary_cache ()
 				break;
 			}
 
-			float speed = 1.0f;
-			RouteTimeAxisView *rtav;
-
-			if (ontrack != 0 && (rtav = dynamic_cast<RouteTimeAxisView*>(ontrack)) != 0 ) {
-				if (rtav->track() != 0) {
-					speed = rtav->track()->speed();
-				}
-			}
-
-			rpos = track_frame_to_session_frame (rpos, speed);
-
 			if (rpos < lpos) {
 				lpos = rpos;
 			}
@@ -845,7 +834,6 @@ Editor::find_next_region (framepos_t frame, RegionPoint point, int32_t dir, Trac
 	boost::shared_ptr<Region> ret;
 	framepos_t rpos = 0;
 
-	float track_speed;
 	framepos_t track_frame;
 	RouteTimeAxisView *rtav;
 
@@ -854,13 +842,7 @@ Editor::find_next_region (framepos_t frame, RegionPoint point, int32_t dir, Trac
 		framecnt_t distance;
 		boost::shared_ptr<Region> r;
 
-		track_speed = 1.0f;
-		if ( (rtav = dynamic_cast<RouteTimeAxisView*>(*i)) != 0 ) {
-			if (rtav->track()!=0)
-				track_speed = rtav->track()->speed();
-		}
-
-		track_frame = session_frame_to_track_frame(frame, track_speed);
+		track_frame = frame;
 
 		if ((r = (*i)->find_next_region (track_frame, point, dir)) == 0) {
 			continue;
@@ -879,9 +861,6 @@ Editor::find_next_region (framepos_t frame, RegionPoint point, int32_t dir, Trac
 			rpos = r->sync_position ();
 			break;
 		}
-
-		// rpos is a "track frame", converting it to "_session frame"
-		rpos = track_frame_to_session_frame(rpos, track_speed);
 
 		if (rpos > frame) {
 			distance = rpos - frame;
@@ -1049,17 +1028,6 @@ Editor::cursor_to_region_point (EditorCursor* cursor, RegionPoint point, int32_t
 		pos = r->sync_position ();
 		break;
 	}
-
-	float speed = 1.0f;
-	RouteTimeAxisView *rtav;
-
-	if ( ontrack != 0 && (rtav = dynamic_cast<RouteTimeAxisView*>(ontrack)) != 0 ) {
-		if (rtav->track() != 0) {
-			speed = rtav->track()->speed();
-		}
-	}
-
-	pos = track_frame_to_session_frame(pos, speed);
 
 	if (cursor == playhead_cursor) {
 		_session->request_locate (pos);
@@ -1240,17 +1208,6 @@ Editor::selected_marker_to_region_point (RegionPoint point, int32_t dir)
 		pos = r->adjust_to_sync (r->first_frame());
 		break;
 	}
-
-	float speed = 1.0f;
-	RouteTimeAxisView *rtav;
-
-	if (ontrack != 0 && (rtav = dynamic_cast<RouteTimeAxisView*>(ontrack)) != 0) {
-		if (rtav->track() != 0) {
-			speed = rtav->track()->speed();
-		}
-	}
-
-	pos = track_frame_to_session_frame(pos, speed);
 
 	loc->move_to (pos, 0);
 }
@@ -3186,8 +3143,6 @@ Editor::separate_regions_between (const TimeSelection& ts)
 
 			/* XXX need to consider musical time selections here at some point */
 
-			double speed = rtv->track()->speed();
-
 			for (list<AudioRange>::const_iterator t = ts.begin(); t != ts.end(); ++t) {
 
 				sigc::connection c = rtv->view()->RegionViewAdded.connect (
@@ -3195,8 +3150,7 @@ Editor::separate_regions_between (const TimeSelection& ts)
 
 				latest_regionviews.clear ();
 
-				playlist->partition ((framepos_t)((*t).start * speed),
-				                     (framepos_t)((*t).end * speed), false);
+				playlist->partition ((*t).start, (*t).end, false);
 
 				c.disconnect ();
 
@@ -3841,16 +3795,11 @@ Editor::trim_region_to_location (const Location& loc, const char* str)
 			return;
 		}
 
-		float speed = 1.0;
 		framepos_t start;
 		framepos_t end;
 
-		if (tav->track() != 0) {
-			speed = tav->track()->speed();
-		}
-
-		start = session_frame_to_track_frame (loc.start(), speed);
-		end = session_frame_to_track_frame (loc.end(), speed);
+		start = loc.start();
+		end = loc.end();
 
 		rv->region()->clear_changes ();
 		rv->region()->trim_to (start, (end - start));
@@ -3901,13 +3850,6 @@ Editor::trim_to_region(bool forward)
 			continue;
 		}
 
-		float speed = 1.0;
-
-		if (atav->track() != 0) {
-			speed = atav->track()->speed();
-		}
-
-
 		boost::shared_ptr<Region> region = arv->region();
 		boost::shared_ptr<Playlist> playlist (region->playlist());
 
@@ -3921,19 +3863,18 @@ Editor::trim_to_region(bool forward)
 				continue;
 			}
 
-			region->trim_end((framepos_t) ( (next_region->first_frame() - 1) * speed));
-			arv->region_changed (PropertyChange (ARDOUR::Properties::length));
+		    region->trim_end (next_region->first_frame() - 1);
+		    arv->region_changed (PropertyChange (ARDOUR::Properties::length));
 		}
 		else {
 
 			next_region = playlist->find_next_region (region->first_frame(), Start, 0);
 
-			if(!next_region){
+			if (!next_region) {
 				continue;
 			}
 
-			region->trim_front((framepos_t) ((next_region->last_frame() + 1) * speed));
-
+			region->trim_front (next_region->last_frame() + 1);
 			arv->region_changed (ARDOUR::bounds_change);
 		}
 
